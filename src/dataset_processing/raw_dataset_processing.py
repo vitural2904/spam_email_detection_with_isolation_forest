566e52e9df903d89eb7ad2fa9275e57a04781b76
vitural2904 <vituralcsgo@gmail.com>
2025-05-12 18:54:47 +0700

    updated 12-5-2025

--- src/dataset_processing/raw_dataset_processing.py
+++ src/dataset_processing/raw_dataset_processing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('Dataset/raw/processed_data.csv')
 
@@ -10,38 +11,26 @@ df_cleaned = df_cleaned.rename(columns={'processed' : 'plaintext'})
 df_normal = df_cleaned[df_cleaned['label'] == 0].copy()
 df_anomalies = df_cleaned[df_cleaned['label'] == 1].copy()
 
-# Create training datasets
+# creating datasets
+total_samples = 20000
 
-# Dataset 1: 7500 spam / 10000 ham
-num_total = 17500
-num_anom1 = 7500
-num_norm1 = num_total - num_anom1
+# --- Dataset 1: 45% spam (9000 spam, 11000 normal) ---
+anom_1 = df_anomalies.sample(n=9000, random_state=42, replace=True)
+norm_1 = df_normal.sample(n=11000, random_state=42, replace=True)
 
-train_1 = pd.concat([
-    df_anomalies.iloc[:num_anom1],
-    df_normal.iloc[:num_norm1]
-], ignore_index=True)
-
+train_1 = pd.concat([anom_1, norm_1], ignore_index=True).sample(frac=1, random_state=42).reset_index(drop=True)
 train_1.to_csv('Dataset/final/training_dataset_1.csv', index=False)
 
-# Dataset 2: 5000 spam / 12500 ham
-num_anom2 = 5000
-num_norm2 = num_total - num_anom2
+# --- Dataset 2: 40% spam (8000 spam, 12000 normal) ---
+anom_2 = df_anomalies.sample(n=8000, random_state=43, replace=True)
+norm_2 = df_normal.sample(n=12000, random_state=43, replace=True)
 
-train_30 = pd.concat([
-    df_anomalies.iloc[:num_anom2],
-    df_normal.iloc[:num_norm2]
-], ignore_index=True)
+train_2 = pd.concat([anom_2, norm_2], ignore_index=True).sample(frac=1, random_state=43).reset_index(drop=True)
+train_2.to_csv('Dataset/final/training_dataset_2.csv', index=False)
 
-train_30.to_csv('Dataset/final/training_dataset_2.csv', index=False)
+# --- Testing dataset: 10000 email, 50% spam ---
+anom_test = df_anomalies.sample(n=5000, random_state=44, replace=True)
+norm_test = df_normal.sample(n=5000, random_state=44, replace=True)
 
-# Create testing dataset (lines haven't used in Dataset 1)
-
-used_anom_indices = df_anomalies.iloc[:num_anom1].index
-used_norm_indices = df_normal.iloc[:num_norm1].index
-
-remaining_anom = df_anomalies.drop(index=used_anom_indices)
-remaining_norm = df_normal.drop(index=used_norm_indices)
-
-testing_dataset = pd.concat([remaining_anom, remaining_norm], ignore_index=True)
-testing_dataset.to_csv('Dataset/final/testing_dataset.csv', index=False)
+test_set = pd.concat([anom_test, norm_test], ignore_index=True).sample(frac=1, random_state=44).reset_index(drop=True)
+test_set.to_csv('Dataset/final/testing_dataset.csv', index=False)
